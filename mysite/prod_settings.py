32e52943b2857d9fdd12deb56e63f69f5d34174b
staspoberezhnik <stas.poberezhnik@gmail.com>
2017-07-23 20:20:31 +0300

    change settings for prod

---
+++ mysite/prod_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+from os import environ
+import dj_database_url
+
+
+DATABASES['default'] = dj_database_url.config()
+
+SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+DEBUG = False
