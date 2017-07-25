aa774ecb14da98916c7203f990ef0811709aecb2
staspoberezhnik <stas.poberezhnik@gmail.com>
2017-07-25 16:44:53 +0300

    change settings for prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,10 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # '/var/www/static/',
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
 MEDIA_URL = '/media/'
