35b535d8efcb4e4602f395b4a925dd94002249b0
staspoberezhnik <stas.poberezhnik@gmail.com>
2017-08-06 18:29:23 +0300

    change settings for prod

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,14 +45,14 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    # 'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
