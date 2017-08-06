d0c04b2f15d1f7c7c9e3168c0ab3a9ec00867661
staspoberezhnik <stas.poberezhnik@gmail.com>
2017-08-06 12:10:15 +0300

    change settings for prod

--- mysite/prod_settings.py
+++ mysite/prod_settings.py
@@ -7,13 +7,23 @@ DATABASE_URL = (
      '6f7b19134939d98c119f13cdf3af8e63ddedb4baa@e' +
      'c2-107-20-255-96.compute-1.amazonaws.com:5432/dalutb2uime2hc')
 
-
 DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
 
 SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'stas-mysite'
+STATICFILES_STORAGE = 'mysite.s3utils.StaticRootS3BotoStorage'
+DEFAULT_FILE_STORAGE = 'mysite.s3utils.StaticRootS3BotoStorage'
+S3_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATIC_URL = S3_URL + 'static/'
+MEDIA_URL = S3_URL + 'media/'
+
 ALLOWED_HOSTS = ['*']
 
+INSTALLED_APPS += ('storages',)
+
 DEBUG = False
-MEDIA_ROOT =  "media_cdn/"
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
