292499deb4ae830f6572ea60b24a2db4da6107d5
staspoberezhnik <stas.poberezhnik@gmail.com>
2017-09-27 17:44:28 +0300

    change templates, Add 4 CBV

--- post/urls.py
+++ post/urls.py
@@ -1,18 +1,23 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from .views import AllPostView, PostByTags, UserPostListView, PostCreateView
 
 urlpatterns = [
 
-    url(r'^$', views.post_list, name='home'),
-    url(r'^create/$', views.post_create, name='post_create'),
+    # url(r'^$', views.post_list, name='home'),
+    url(r'^$', AllPostView.as_view(), name='home'),
+    url(r'^create/$', PostCreateView.as_view(), name='post_create'),
+    # url(r'^create/$', views.post_create, name='post_create'),
     url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^(?P<id>\d+)/addcomment/$', views.add_comment, name='add_comment'),
     url(r'^(?P<id>\d+)/edit/$', views.post_update, name='edit_post'),
     url(r'^user_list/$', views.users_list, name='users_list'),
-    url(r'^(?P<id>\d+)/user_post_list/$', views.user_post_list, name='user_post_list'),
+    # url(r'^(?P<id>\d+)/user_post_list/$', views.user_post_list, name='user_post_list'),
+    url(r'^(?P<id>\d+)/user_post_list/$',UserPostListView.as_view(), name= 'user_post_list'),
     url(r'^(?P<id>\d+)/user_comments_list/$', views.user_comments_list, name='user_comments_list'),
-    url(r'^post_by_tags/(?P<tag>[-\w]+)/$', views.post_by_tags, name='post_by_tags'),
+    # url(r'^post_by_tags/(?P<tag>[-\w]+)/$', views.post_by_tags, name='post_by_tags'),
+    url(r'^post_by_tags/(?P<tag>[-\w]+)/$', PostByTags.as_view(), name='post_by_tags'),
     url(r'^(?P<id>\d+)/delete/$', views.post_delete, name='delete_post'),
 
 
--- post/views.py
+++ post/views.py
@@ -2,66 +2,114 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.views.generic import ListView, CreateView
 # from . import filters
 from .filters import PostFilter
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Comment
 from .forms import CommentForm, PostForm
 # Create your views here.
 
 
-def post_list(request):
-    username = None
-    if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
-        username = auth.get_user(request).username
+class AllPostView(ListView):
 
-    query_post_list = Post.objects.all().order_by("-date_created")
-    paginator = Paginator(query_post_list, 10)  # Show 10  per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
+    template_name = 'home_page.html'
+    model = Post
+    context_object_name = 'post_list'
+    paginate_by = 10
 
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        queryset = paginator.page(1)
-    except EmptyPage:
-        queryset = paginator.page(paginator.num_pages)
+    def get_queryset(self):
+        return Post.objects.all().order_by("-date_created")
 
-    context = {
-        'post_list': queryset,
-        'username': username,
-        'page_request_var': page_request_var,
-    }
-    return render(request, 'home_page.html', context)
+    def get_context_data(self, **kwargs):
+
+        context = super(AllPostView, self).get_context_data(**kwargs)
+        username = None
+        if self.request.user.is_staff\
+            or self.request.user.is_superuser\
+                or self.request.user.is_authenticated:
+            username = auth.get_user(self.request).username
+
+        page_request_var = 'page'
+
+        context['username'] = username
+        context['page_request_var'] = page_request_var
+        return context
 
 
-def post_create(request):
-    user = request.user
-    form = PostForm(request.POST or None, request.FILES or None)
-    if form.is_valid():
+# def post_list(request):
+#     username = None
+#     if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
+#         username = auth.get_user(request).username
+#
+#     query_post_list = Post.objects.all().order_by("-date_created")
+#     paginator = Paginator(query_post_list, 10)  # Show 10  per page
+#     page_request_var = 'page'
+#     page = request.GET.get(page_request_var)
+#
+#     try:
+#         queryset = paginator.page(page)
+#     except PageNotAnInteger:
+#         queryset = paginator.page(1)
+#     except EmptyPage:
+#         queryset = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         'post_list': queryset,
+#         'username': username,
+#         'page_request_var': page_request_var,
+#     }
+#     return render(request, 'home_page.html', context)
+
+class PostCreateView(CreateView):
+    form_class = PostForm
+    template_name = 'post_creation_form.html'
+
+    def form_valid(self, form):
         instance = form.save(commit=False)
-        instance.user = User.objects.get(id=user.pk)
+        instance.user = self.request.user
         instance.save()
         form.save_m2m()
-        return HttpResponseRedirect(instance.get_absolute_url())
-    context = {
-        "form": form,
-        'username': auth.get_user(request).username,
-    }
-    return render(request, 'post_creation_form.html', context)
+        return redirect(instance.get_absolute_url())
+
+    def get_context_data(self, **kwargs):
+        context = super(PostCreateView, self).get_context_data(**kwargs)
+        context['username'] = self.request.user.username
+        return context
+
+    def form_invalid(self, form):
+        return redirect('post: post_create')
+
+# def post_create(request):
+#     user = request.user
+#     form = PostForm(request.POST or None, request.FILES or None)
+#     if form.is_valid():
+#         instance = form.save(commit=False)
+#         instance.user = User.objects.get(id=user.pk)
+#         instance.save()
+#         form.save_m2m()
+#         return HttpResponseRedirect(instance.get_absolute_url())
+#     context = {
+#         "form": form,
+#         'username': auth.get_user(request).username,
+#     }
+#     return render(request, 'post_creation_form.html', context)
 
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
     user = request.user
-    if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
+    if request.user.is_staff or\
+            request.user.is_superuser or\
+            request.user.is_authenticated:
         username = auth.get_user(request).username
     else:
         username = None
 
     if instance.user_id == user.pk:
         if request.POST:
-            form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+            form = PostForm(request.POST or None,
+                            request.FILES or None, instance=instance)
             if form.is_valid():
                 instance = form.save(commit=False)
                 instance.save()
@@ -91,7 +139,9 @@ def post_detail(request, id):
     can_edit = None
     can_delete = None
 
-    if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
+    if request.user.is_staff or\
+            request.user.is_superuser or\
+            request.user.is_authenticated:
         username = auth.get_user(request).username
         comment_form = CommentForm()
 
@@ -128,29 +178,35 @@ def add_comment(request, id):
     return redirect(post.get_absolute_url())
 
 
-def post_by_tags(request, tag):
-    username = None
-    if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
-        username = auth.get_user(request).username
+class PostByTags(AllPostView):
+    paginate_by = 5
 
-    query_post_list = Post.objects.filter(tags__slug=tag).order_by("-date_created")
-    paginator = Paginator(query_post_list, 10)  # Show 10  per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['tag']).order_by("-date_created")
 
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        queryset = paginator.page(1)
-    except EmptyPage:
-        queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        'post_list': queryset,
-        'username': username,
-        'page_request_var': page_request_var,
-    }
-    return render(request, 'home_page.html', context)
+# def post_by_tags(request, tag):
+#     username = None
+#     if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
+#         username = auth.get_user(request).username
+#
+#     query_post_list = Post.objects.filter(tags__slug=tag).order_by("-date_created")
+#     paginator = Paginator(query_post_list, 10)  # Show 10  per page
+#     page_request_var = 'page'
+#     page = request.GET.get(page_request_var)
+#
+#     try:
+#         queryset = paginator.page(page)
+#     except PageNotAnInteger:
+#         queryset = paginator.page(1)
+#     except EmptyPage:
+#         queryset = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         'post_list': queryset,
+#         'username': username,
+#         'page_request_var': page_request_var,
+#     }
+#     return render(request, 'home_page.html', context)
 
 
 def users_list(request):
@@ -162,36 +218,55 @@ def users_list(request):
     return render(request, 'users_list.html', context)
 
 
-def user_post_list(request, id):
+class UserPostListView(AllPostView):
+    paginate_by = 10
 
-    username = None
-    if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
-        username = auth.get_user(request).username
+    def get_queryset(self):
+        queryset = PostFilter(self.request.GET,
+                              queryset=Post.objects.
+                              filter(user_id=self.kwargs['id']).
+                              order_by("-date_created"))
+        return queryset.qs
 
-    post_filtered = PostFilter(
-        request.GET,
-        queryset=Post.objects.filter(user_id=id).order_by("-date_created")
-    )
+    def get_context_data(self, **kwargs):
+        context = super(UserPostListView, self).get_context_data(**kwargs)
+        model_filtered = PostFilter(self.request.GET,
+                                    queryset=Post.objects.
+                                    filter(user_id=self.kwargs['id']).
+                                    order_by("-date_created"))
+        context['filter'] = model_filtered
+        return context
 
-    # query_post_list = Post.objects.filter(post_user_id=id).order_by("-post_date_created")
-    paginator = Paginator(post_filtered.qs, 5)  # Show 10  per page
-    page_request_var = 'page'
-    page = request.GET.get(page_request_var)
 
-    try:
-        queryset = paginator.page(page)
-    except PageNotAnInteger:
-        queryset = paginator.page(1)
-    except EmptyPage:
-        queryset = paginator.page(paginator.num_pages)
-
-    context = {
-        'filter': post_filtered,
-        'post_list': queryset,
-        'username': username,
-        'page_request_var': page_request_var,
-    }
-    return render(request, 'home_page.html', context)
+# def user_post_list(request, id):
+#
+#     username = None
+#     if request.user.is_staff or request.user.is_superuser or request.user.is_authenticated:
+#         username = auth.get_user(request).username
+#
+#     post_filtered = PostFilter(
+#         request.GET,
+#         queryset=Post.objects.filter(user_id=id).order_by("-date_created")
+#     )
+#
+#     paginator = Paginator(post_filtered.qs, 5)  # Show 10  per page
+#     page_request_var = 'page'
+#     page = request.GET.get(page_request_var)
+#
+#     try:
+#         queryset = paginator.page(page)
+#     except PageNotAnInteger:
+#         queryset = paginator.page(1)
+#     except EmptyPage:
+#         queryset = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         'filter': post_filtered,
+#         'post_list': queryset,
+#         'username': username,
+#         'page_request_var': page_request_var,
+#     }
+#     return render(request, 'home_page.html', context)
 
 
 def user_comments_list(request, id):
